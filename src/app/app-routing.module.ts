ba27624ecb0d327c4ca89e5fa7a276be9bcd45aa
unknown <Shirling98>
2021-11-02 13:42:59 +0700

    FRNT-7652 add rout and page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeBooksComponent} from "./home-page/home-books.component";
+import {BookComponent} from "./book/book.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeBooksComponent},
+  {path: 'book/create', component: BookComponent},
+  {path: 'book/edit/:id', component: BookComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { HomeBooksComponent } from './home-page/home-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    HomeBooksComponent
   ],
   imports: [
     BrowserModule,
