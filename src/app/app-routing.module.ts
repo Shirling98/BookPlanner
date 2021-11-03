85e65c25712a3d6993394337c2144de1cc61ba8e
Shirling98 <shirling.rita@gmail.com>
2021-11-03 09:59:50 +0700

    FRNT-7655 change components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: '/books', pathMatch: 'full'},
+  {path: '', redirectTo: '/books/booksList', pathMatch: 'full'},
   {path: 'books', loadChildren: () => import('./books/books.module').then((m) => m.BooksModule)}
 ];
 
--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
-import {HomePageComponent} from "./home-page/home-page.component";
+import {HomePageComponent} from "./shared/components/home-page/home-page.component";
 import {BookComponent} from "./book/book.component";
 import { BooksListComponent } from './books-list/books-list.component';
 
@@ -16,7 +16,7 @@ import { BooksListComponent } from './books-list/books-list.component';
     RouterModule.forChild([
       {
         path: '', component: HomePageComponent, children: [
-         // {path: 'books', component: BooksListComponent},
+          {path: 'booksList', component: BooksListComponent},
           {path: 'create', component: BookComponent},
           {path: 'edit/:id', component: BookComponent}
         ]
